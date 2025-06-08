516b2d56c936a8ccb810e75f3fc1ab62d15aab34
wai4u <kagemeka1+wai4u@gmail.com>
2025-06-09 00:20:42 +0900

    a

--- work/1.cpp
+++ work/1.cpp
@@ -1,42 +1,5 @@
 #include"io.hpp"
-//primal-dual
-//fenwick_20250608_5.hpp で言及した通り、サイズ初期化以外の初期化手段を廃止
-//代わりにlazy buildをする
-//ただしadd時のbuild保留optionを廃止。initを読んだ後[i]を呼ぶまでの間だけO(1)addされる。
-//https://judge.yosupo.jp/submission/291769
-template<class T>
-struct fenwick{
-	int n=0;
-	vector<T>d;
-	bool built=0;
-	fenwick(){}
-	fenwick(int n){init(n);}
-	void init(int n){
-		this->n=n;
-		d.assign(n,T{});
-		built=0;
-	}
-	void build(){
-		if(built)return;
-		built=1;
-		for(int i=0;i<n;i++){
-			int j=i|i+1;
-			if(j<n)d[j]+=d[i];
-		}
-	}
-	void add(int i,T x){
-		assert(0<=i);
-		if(!built){d[i]+=x;return;}
-		while(i<n)d[i]+=x,i|=i+1;
-	}
-	T operator[](int i){
-		assert(i<n);
-		build();
-		T y{};
-		while(i>=0)y+=d[i],i&=i+1,i--;
-		return y;
-	}
-};
+#include"fenwick.hpp"
 int main(){
 	int n,q;
 	cin>>n>>q;
---
+++ work/cp/fenwick.hpp
@@ -0,0 +1,1 @@
+#include"fenwick_20250608_6.hpp"
