7f92825c21d2ed47b95e04a4501188dada6df440
wai4u <kagemeka1+wai4u@gmail.com>
2025-06-21 09:19:32 +0900

    a

---
+++ work/lib/combination_20250621_1.hpp
@@ -0,0 +1,23 @@
+//https://atcoder.jp/contests/agc054/submissions/66908452
+template<class T>
+struct combination{
+	vector<T>fact{1},ifact{1};
+	void build(int n){
+		int m=fact.size();
+		if(m>n)return;
+		n=max(n,m+m);
+		fact.resize(n+1);
+		ifact.resize(n+1);
+		for(int i=m;i<=n;i++)fact[i]=fact[i-1]*i;
+		ifact[n]=1/fact[n];
+		for(int i=n;i-->m;)ifact[i]=ifact[i+1]*(i+1);
+	}
+	T P(int n,int k){
+		if(k==0)return 1;
+		assert(n>=0&&k>=0);//n<0には非対応
+		if(k>n)return 0;
+		build(n);
+		return fact[n]*ifact[n-k];
+	}
+	T C(int n,int k){return P(n,k)*ifact[k];}
+};
